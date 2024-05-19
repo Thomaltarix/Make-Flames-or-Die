dac8e3a71af8d53951916b1ed6d5a908fedf53ed
thomaltarix <thomas.boue@epitech.eu>
2024-05-19 15:50:16 +0200

    :truck: Move a method in a better place

--- lib/main.dart
+++ lib/main.dart
@@ -143,6 +143,13 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget _buildBody() {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.8,
+      width: MediaQuery.of(context).size.width,
+    );
+  }
+  
   Widget _buildBottomNavBar() {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.10,
@@ -156,13 +163,6 @@ class _MyHomePageState extends State<MyHomePage> {
             );
           }
       ),
-    );
-  }
-
-  Widget _buildBody() {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.8,
-      width: MediaQuery.of(context).size.width,
     );
   }
 
